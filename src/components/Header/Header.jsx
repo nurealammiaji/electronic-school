f346344b6604fbde11f338ecf27ce085c56783ff
nurealammiaji <nurealam.miaji@gmail.com>
2023-08-13 00:03:58 +0600

    nav bar style modified

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,12 +7,12 @@ const Header = () => {
             <div className="my-2 w-52">
                 <Link to="/"><img src={logo} alt="" /></Link>
             </div>
-            <div className="text-xl my-2 [&>*]:p-4 [&>*:hover]:bg-green-500">
+            <div className="text-xl my-2 [&>*]:py-2 [&>*]:px-4 [&>*:hover]:bg-green-500 [&>*:hover]:rounded-3xl">
                 <Link to="/books">Books</Link>
                 <Link to="/courses">Courses</Link>
                 <Link to="/others">Others</Link>
                 <Link to="/blog">Blog</Link>
-                <Link to="/signin">Sign in</Link>
+                <Link className="text-white bg-green-500 rounded-3xl" to="/signin">Sign in</Link>
             </div>
         </nav>
     );
--- src/components/Home/FirstSection.jsx
+++ src/components/Home/FirstSection.jsx
@@ -1,21 +1,22 @@
-import image from "../../assets/photos/Illustration.png"
+import { Link } from "react-router-dom";
+import image from "../../assets/photos/Illustration.png";
 
 const FirstSection = () => {
     return (
         <div className="bg-green-100">
-            <div className="flex items-center gap-2">
+            <div className="flex items-center gap-8">
                 <div>
                     <br /><br /><br /><br />
-                    <p className="mb-5 text-3xl text-orange-500">Are you ready to learn ?</p>
+                    <p className="mb-5 text-2xl text-orange-500">Are you ready to learn ?</p>
                     <br />
-                    <div className="font-extrabold text-7xl">
+                    <div className="text-5xl font-extrabold">
                         <h1 className="mb-3">Learn With fun</h1>
                         <h1>on <span className="text-green-500">any schedule</span></h1>
                     </div>
-                    <br />
-                    <p className="text-2xl">Lorem ipsum, dolor sit amet <br /> consectetur adipisicing elit. Adipisci, laborum.</p>
-                    <br />
-                    <button className="px-8 py-4 text-2xl text-white bg-green-500 rounded-3xl">Get Started</button>
+                    <br /><br />
+                    <p className="text-xl">Lorem ipsum, dolor sit amet <br /> consectetur adipisicing elit. Adipisci, laborum.</p>
+                    <br /><br />
+                    <Link to="/courses"><button className="py-2 text-xl text-white bg-green-500 px-7 rounded-3xl">Get Started</button></Link>
                 </div>
                 <div className="w-6/12">
                     <img className="w-auto" src={image} alt="" />
