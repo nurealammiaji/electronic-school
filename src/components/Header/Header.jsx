6bd70c5fb6d6a3f63edafbde285302fe6e97bd3b
nurealammiaji <nurealam.miaji@gmail.com>
2023-08-12 19:19:28 +0600

    tailwind css setup added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,12 +3,14 @@ import logo from '../../assets/icons/Logo.png';
 
 const Header = () => {
     return (
-        <nav>
-            <div>
+        <nav className="flex items-center justify-between">
+            <div className="w-80">
                 <img src={logo} alt="" />
             </div>
-            <div>
+            <div className="text-xl">
                 <Link to="/books">Books</Link>
+                <Link to="/courses">Courses</Link>
+                <Link to="/others">Others</Link>
             </div>
         </nav>
     );
