2cdc95ceeae352c960c4407108e68cbb2cbd80ef
nurealammiaji <nurealam.miaji@gmail.com>
2023-08-12 21:13:13 +0600

    nav bar style modified

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,11 +3,11 @@ import logo from '../../assets/icons/Logo.png';
 
 const Header = () => {
     return (
-        <nav className="flex items-center justify-between">
-            <div className="w-80">
+        <nav className="flex items-center justify-between bg-green-100">
+            <div className="my-2 w-52">
                 <Link to="/"><img src={logo} alt="" /></Link>
             </div>
-            <div className="text-xl">
+            <div className="text-xl my-2 [&>*]:p-4 [&>*:hover]:bg-green-500">
                 <Link to="/books">Books</Link>
                 <Link to="/courses">Courses</Link>
                 <Link to="/others">Others</Link>
